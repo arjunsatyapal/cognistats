d92df2d294abf95fceb401470d3d3aa94f1adcc1
Nick Winter <livelily@gmail.com>
2011-06-07 17:15:52 -0700

    Edited some styles a tiny bit.

--- app/home/handlers.py
+++ app/home/handlers.py
@@ -5,18 +5,34 @@
 
     document it
 """
+import os
+import logging
+
 from tipfy.app import Response
 from tipfy.handler import RequestHandler
 from tipfyext.jinja2 import Jinja2Mixin
+import tipfyext.jinja2
+from tipfyext.jinja2 import default_config
 #from tipfy.ext.auth import AppEngineAuthMixin, user_required
+
+
+class PageHandler(RequestHandler, Jinja2Mixin):
+    def get(self, template=None):
+        """Simply returns a rendered template with an enigmatic salutation."""
+        context = {}
+        path = os.path.join(tipfyext.jinja2.default_config['templates_dir'],
+                            template + '.html')
+        if not os.path.exists(path):
+            return Response(self.jinja2.render_template(
+                    self, 'error_handlers/404.html', **context), status=404)
+        return self.render_response(template + '.html', **context)
+
 
 class FrontPageHandler(RequestHandler, Jinja2Mixin): 
     def get(self):
         """Simply returns a rendered template with an enigmatic salutation."""
-        context = {
-            'message': 'Hello, World! -- this is the front page',
-        }
-        return self.render_response('hello_world.html', **context)
+        context = {}
+        return self.render_response('front.html', **context)
 
 
 class HomePageHandler(RequestHandler, Jinja2Mixin):#, AppEngineAuthMixin):
@@ -27,4 +43,5 @@ class HomePageHandler(RequestHandler, Jinja2Mixin):#, AppEngineAuthMixin):
         context = {
             'message': 'You done logged in!',
         }
-        return self.render_response('hello_world.html', **context)
+        return self.render_response('front.html', **context)
+
--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from tipfy.routing import Rule
 
 rules = [
     Rule('/', name='front-page', handler='home.handlers.FrontPageHandler'),
+    Rule('/<template>', name='page', handler='home.handlers.PageHandler'),
     Rule('/home', name='home-page', handler='home.handlers.HomePageHandler'),
     Rule('/hello', name='hello-world', handler='hello_world.handlers.HelloWorldHandler'),
     Rule('/pretty', name='hello-world-pretty', handler='hello_world.handlers.PrettyHelloWorldHandler'),
