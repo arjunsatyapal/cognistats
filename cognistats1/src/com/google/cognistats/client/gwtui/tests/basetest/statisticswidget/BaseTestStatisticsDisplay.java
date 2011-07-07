461617dfd8d55faf62124bd22beb1590bb71e341
Arjun Satyapal <arjunsataypal@gmail.com>
2011-07-07 12:03:41 -0700

    removing old baseTestStat.

--- cognistats1/src/com/google/cognistats/client/gwtui/tests/basetest/statisticswidget/BaseTestStatisticsDisplay.java
+++
@@ -1,8 +0,0 @@
-package com.google.cognistats.client.gwtui.tests.basetest.statisticswidget;
-
-import com.google.cognistats.client.gwtui.mvpinterfaces.Display;
-
-public interface BaseTestStatisticsDisplay extends Display {
-	public void setTestTimeCurrent(long timeInMillis);
-	public void setTestTimeAllTime(long timeInMillis);
-}
