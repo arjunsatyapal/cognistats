5d71f1f0cb8c44702895916458b1c353dcf70d0c
Yoni Donner <yonidonner@gmail.com>
2011-08-19 13:29:57 -0700

    Some more improvements to digit span

--- cognistats1/src/com/google/cognistats/client/gwtui/tests/span/DigitSpanPresenter.java
+++ cognistats1/src/com/google/cognistats/client/gwtui/tests/span/DigitSpanPresenter.java
@@ -15,20 +15,30 @@ public class DigitSpanPresenter extends SpanPresenter implements Presenter {
 
 	protected DigitSpanTestDisplay digitSpanTestWidget;
 	protected ArrayList<Integer> currentSequence;
-	protected boolean isBackwards;
+	protected boolean isBackwards, isAudio, isVisual;
 	protected SpanStatistic spanStatistic;
 
 	public DigitSpanPresenter(DigitSpanTestDisplay testWidget, BaseStatisticWidgetPresenter statPresenter) {
 		super(testWidget.getSpanTestView(), statPresenter);
 		digitSpanTestWidget = testWidget;
 		isBackwards = false;
+		isAudio = false;
+		isVisual = true;
 		setupUi();
 	}
 	
-	protected void setBackwards(boolean isBackwards) {
+	public void setBackwards(boolean isBackwards) {
 		this.isBackwards = isBackwards;
 	}
 	
+	public void setAudio(boolean isAudio) {
+		this.isAudio = isAudio;
+	}
+
+	public void setVisual(boolean isVisual) {
+		this.isVisual = isVisual;
+	}
+
 	@Override
 	public String getInstructions() {
 		return "Repeat the sequence of digits.";
@@ -36,7 +46,9 @@ public class DigitSpanPresenter extends SpanPresenter implements Presenter {
 
 	@Override
 	protected void displaySequenceElement(int number) {
-		digitSpanTestWidget.setDigit(currentSequence.get(number));
+		if (isVisual) {
+			digitSpanTestWidget.setDigit(currentSequence.get(number));
+		}
 	}
 
 	@Override
@@ -98,8 +110,15 @@ public class DigitSpanPresenter extends SpanPresenter implements Presenter {
 	public void userInputDone() {
 		StringBuffer correctAnswer = new StringBuffer();
 		int i;
-		for (i = 0; i < currentSequence.size(); ++i) {
-			correctAnswer.append(new Character((char) ('0' + currentSequence.get(i))));
+		if (isBackwards) {
+			for (i = currentSequence.size() - 1; i >= 0; --i) {
+				correctAnswer.append(new Character((char) ('0' + currentSequence.get(i))));
+			}			
+		}
+		else {
+			for (i = 0; i < currentSequence.size(); ++i) {
+				correctAnswer.append(new Character((char) ('0' + currentSequence.get(i))));
+			}
 		}
 		inputDone(correctAnswer.toString().equals(digitSpanTestWidget.getInputText()));
 		digitSpanTestWidget.setInputVisible(false);
